826d92da998704cb7f780acb711e74081539e94e
pcouy <couy.pierre@gmail.com>
2023-04-10 16:10:43 +0200

    Initial commit

---
+++ lib/jekyll-chatgpt.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "jekyll-chatgpt/version"
+
+module JekyllChatgpt
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jekyll-chatgpt/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module JekyllChatgpt
+  VERSION = "0.1.0"
+end
